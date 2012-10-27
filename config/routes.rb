555857475282ede2e93c84a7724f871b47cb0910
Pablo Castelnovo <pablonvz@gmail.com>
2012-10-27 19:20:44 -0300

    Change devise path names

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Stock::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :path_names => {
+    sign_in: 'login',
+    sign_out: 'logout',
+    sign_up: 'register'
+  }
 
   root :to => "application#index"
 
