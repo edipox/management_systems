5f97cb9fad84c3dd52575d4387eb7be03fd3523c
Pablo Castelnovo <pablonvz@gmail.com>
2012-10-27 18:38:01 -0300

    Gem devise configured

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Stock::Application.routes.draw do
+  devise_for :users
+
+  root :to => "application#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
