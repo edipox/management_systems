3fd6b7017f2e1958cadb141a03ee38a8eca17090
Pablo Castelnovo <pablonvz@gmail.com>
2012-11-04 11:07:46 -0300

    Update db setup file

--- db/setup.rb
+++ db/setup.rb
@@ -7,8 +7,8 @@ ACL::Entity.create!({
   name: 'Componentes',
   const: 'Component'
 })
-
-Role.create!({ name: 'Empleado' })
+# For test permissions creation, create a Role after a Component
+employee_role = Role.create!({ name: 'Empleado' })
 
 User.create!({
   email: 'admin@site.com',
@@ -16,3 +16,15 @@ User.create!({
   password_confirmation: 'contrasenha',
   role: admin_role
 })
+
+employee = User.create!({
+  email: 'employee@site.com',
+  password: 'contrasenha',
+  password_confirmation: 'contrasenha',
+  role: employee_role
+})
+
+employee.role.permissions.each do |permission|
+  permission.enabled = true if permission.action == 'read'
+  permission.save
+end
