b88bd71015d7b3e91578e2fce3d368771fe572d1
Edipo Vinicius da Silva <edipo@ycube.net>
2012-11-06 11:35:51 -0300

    Style modifications added

--- app/controllers/components/categories_controller.rb
+++ app/controllers/components/categories_controller.rb
@@ -59,12 +59,10 @@ class Components::CategoriesController < ApplicationController
         format.json { render json: @components_category.errors, status: :unprocessable_entity }
         format.js{ 
             render :nothing => true
-        
         ## CODIGO QUE PUEDE SER DE UTILIDAD
 #              render :update do |page|
 #                  page.redirect_to your_url_for_parameters_or_string
 #              end
-            
         }
 #        {     
 #           Variable utilizada para re renderizar index
