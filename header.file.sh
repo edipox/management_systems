ff256adc2601ecf1271ea2e2451641f2e4523609
Edipo Vinicius da Silva <edipo@ycube.net>
2012-11-11 22:14:26 -0300

    Generators scripts created

---
+++ header.file.sh
@@ -0,0 +1,7 @@
+products/composition name:string description:string
+requests/purchases/component status_id:string user_id:string transaction_id:string
+requests/transferences/component status_id:string user_id:string transaction_id:string 
+requests/transferences/product status_id:string transaction_id:string user_id:string 
+requests/devolutions/product transaction_id:string user_id:string reason:string status_id:string
+orders/production transaction_id:string status_id:string user_id:string 
+requests/devolutions/component transaction_id:string status_id:string user_id:string reason:string
