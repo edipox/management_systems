df6f74e4ecfe79eb7574c55ba6aac3f4d1751bdb
Pablo Castelnovo <pablonvz@gmail.com>
2012-11-13 14:49:00 -0300

    Stock components basic view

--- app/controllers/stock/components_controller.rb
+++ app/controllers/stock/components_controller.rb
@@ -1,9 +1,12 @@
 class Stock::ComponentsController < ApplicationController
   layout "stock_components"
   def index
-    @existances = Stocks::Component.paginate(:page => params[:page])
+
+  end
+  
+  def list
+    @components = Components::Item.paginate(:page => params[:page])
     respond_to do |format|
-      format.html
       format.js
     end
   end
