18ab301c132fea73eabb75b25e17f58b43e4e306
Pablo Castelnovo <pablonvz@gmail.com>
2012-11-15 10:27:34 -0300

    Move script into right folder

--- script/header.file.sh
+++ script/header.file.sh
@@ -1,7 +1,7 @@
-rails g scaffold products/composition name:string description:string -f
-rails g scaffold requests/purchases/component status_id:string user_id:string transaction_id:string -f
-rails g scaffold requests/transferences/component status_id:string user_id:string transaction_id:string  -f
-rails g scaffold requests/transferences/product status_id:string transaction_id:string user_id:string  -f
-rails g scaffold requests/devolutions/product transaction_id:string user_id:string reason:string status_id:string -f
-rails g scaffold orders/production transaction_id:string status_id:string user_id:string  -f
-rails g scaffold requests/devolutions/component transaction_id:string status_id:string user_id:string reason:string -f
+rails g scaffold products/composition name:string description:string
+rails g scaffold requests/purchases/component status_id:string user_id:string transaction_id:string
+rails g scaffold requests/transferences/component status_id:string user_id:string transaction_id:string 
+rails g scaffold requests/transferences/product status_id:string transaction_id:string user_id:string 
+rails g scaffold requests/devolutions/product transaction_id:string user_id:string reason:string status_id:string
+rails g scaffold orders/production transaction_id:string status_id:string user_id:string 
+rails g scaffold requests/devolutions/component transaction_id:string status_id:string user_id:string reason:string
