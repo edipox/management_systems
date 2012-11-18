71fb64c47ee19f6fe400d6d43808117d66aa4ce7
Edipo Vinicius da Silva <edipo@ycube.net>
2012-11-18 14:53:25 -0300

    Issue #161 components transferences request CRUD

--- app/models/components/item.rb
+++ app/models/components/item.rb
@@ -7,6 +7,7 @@ class Components::Item < ActiveRecord::Base
   has_many :production_stocks, :foreign_key => :component_id, :class_name => ::Stocks::Production.to_s
   has_paper_trail
   
+  has_many :requests_transferences_components_detail, :class_name => Requests::Transferences::Components::Detail.to_s
   #has_many :requests_devolutions_components_details, :foreign_key => :component_id, :class_name => "Requests::Devolutions::Components::Detail"
   
 end
