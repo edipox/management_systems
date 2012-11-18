2d6280373522e3b2f72202d3ed166a5f79082ef6
Edipo Vinicius da Silva <edipo@ycube.net>
2012-11-17 21:07:25 -0300

    Components' model CRUD removed becouse it is not needed

--- app/models/components/model.rb
+++
@@ -1,7 +0,0 @@
-class Components::Model < ActiveRecord::Base
-   has_paper_trail
-  attr_accessible :brand_id, :description, :name
-  validates :name, :presence => true #, :length => { :minimum => 2 }  
-  validates :brand_id, :presence => true #, :length => { :minimum => 2 }  
-  belongs_to :brand, :class_name => Components::Brand.to_s
-end
