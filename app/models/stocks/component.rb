af3fa1b09a64c396bd383fe1774fc04c47d61fb4
Edipo Vinicius da Silva <edipo@ycube.net>
2012-11-25 23:46:24 -0300

    Request purchases components issue #181 working

--- app/models/stocks/component.rb
+++ app/models/stocks/component.rb
@@ -14,10 +14,18 @@ class Stocks::Component < ActiveRecord::Base
     sum = component.raw_material_stocks.reduce { |s1, s2| s1.quantity += s2.quantity; s1 }
     if component.minimum_quantity > sum.quantity
       missing_component_qty = component.minimum_quantity - sum.quantity
-      
-      puts "Missing component quantity "
-      puts missing_component_qty
-      ### Generate PurchaseOrder's
+      request_purchase = Requests::Purchases::Component.create!({
+        status_id: AppConfig.find('open_status_id').value,
+        user_id: AppConfig.find('system_user_id').value,
+        #temp
+        transaction_id: 'nil'
+        #temp
+      }) 
+      Requests::Purchases::Components::Detail.create!({
+        header_id: request_purchase.id,
+        component: component,
+        quantity: missing_component_qty
+      })
     end
   end
 end
