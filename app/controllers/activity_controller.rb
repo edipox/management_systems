c2d2eb64d65e03296b3b9cc00aef3db04489f222
Edipo Vinicius da Silva <edipo@ycube.net>
2012-11-27 08:06:51 -0300

    Versions view improved

--- app/controllers/activity_controller.rb
+++ app/controllers/activity_controller.rb
@@ -11,11 +11,20 @@ class ActivityController < ApplicationController
 
   def show
     version = Version.find(params[:id])
+    
     version_class = version.item_type.constantize
     if version_class.respond_to? :with_deleted
       version_class = version_class.with_deleted
     end
-    @current = version_class.find version.item_id
+    
+    next_version = Version.where("id > :id and item_id = :item_id", {id: version.id, item_id: version.item_id}).first
+    
+    if next_version
+      @current = next_version.reify
+    else
+      @current = version_class.find version.item_id
+    end
+    
     @versioned = version.reify
 
     respond_to do |format|
